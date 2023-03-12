5209d7a55827d19b8213b1c34669c85056c91562
AlinaSyniavska <alina22syniavska@gmail.com>
2023-03-12 16:57:53 +0200

    change logic addToCart (same items don't add to a cart)

--- src/redux/slices/cart.slice.ts
+++ src/redux/slices/cart.slice.ts
@@ -38,54 +38,82 @@ const cartSlice = createSlice({
   reducers: {
     addToCart: (state, action) => {
       const goods = action.payload.goods;
-      const goodsToOrder = {
-        productId: goods._id,
-        count: 1,
-        cost: goods.price,
-      };
       const cart = localStorageHelper.getArrayFromLocalStorage(localStorageItemsEnum.CART) as IProduct[];
       const order = localStorageHelper.getArrayFromLocalStorage(localStorageItemsEnum.ORDER) as IProductOrdered[];
+      let goodsToOrder = {} as IProductOrdered;
+      const orderItem = order.find((item) => item.productId === goods._id);
 
-      state.goods.push(goods);
-      state.userOrder.push(goodsToOrder);
+      if (orderItem) {
+        Object.assign(goodsToOrder, orderItem, { count: orderItem.count + 1 });
+        state.userOrder.splice(
+            state.userOrder.findIndex(item => item === orderItem),
+            1,
+            goodsToOrder
+        );
+        order.splice(
+            order.findIndex(item => item === orderItem),
+            1,
+            goodsToOrder
+        );
+      } else {
+        goodsToOrder = {
+          productId: goods._id,
+          count: 1,
+          cost: goods.price,
+        };
 
-      cart.push(goods);
+        state.goods.push(goods);
+        state.userOrder.push(goodsToOrder);
+        cart.push(goods);
+        order.push(goodsToOrder);
+      }
+
       localStorageHelper.setArrayToLocalStorage(localStorageItemsEnum.CART, cart);
-
-      order.push(goodsToOrder);
       localStorageHelper.setArrayToLocalStorage(localStorageItemsEnum.ORDER, order);
     },
 
     changeOrder: (state, action) => {
       const { itemId, curCount } = action.payload.data;
 
-      const order = localStorageHelper.getArrayFromLocalStorage(localStorageItemsEnum.ORDER) as IProductOrdered[];
+      const order = localStorageHelper.getArrayFromLocalStorage(
+        localStorageItemsEnum.ORDER
+      ) as IProductOrdered[];
 
-      const singleGoodsIndex = order.findIndex((i: IProductOrdered) => i.productId === itemId);
+      const singleGoodsIndex = order.findIndex(
+        (i: IProductOrdered) => i.productId === itemId
+      );
       order[singleGoodsIndex].count = curCount;
 
-      state.userOrder.splice(
-        singleGoodsIndex,
-        1,
-        order[singleGoodsIndex]
-      );
+      state.userOrder.splice(singleGoodsIndex, 1, order[singleGoodsIndex]);
 
-      localStorageHelper.setArrayToLocalStorage(localStorageItemsEnum.ORDER, order);
+      localStorageHelper.setArrayToLocalStorage(
+        localStorageItemsEnum.ORDER,
+        order
+      );
     },
 
     changeOrderDeleteRecord: (state, action) => {
       const { itemId } = action.payload.data;
 
-      const order = localStorageHelper.getArrayFromLocalStorage(localStorageItemsEnum.ORDER) as IProductOrdered[];
+      const order = localStorageHelper.getArrayFromLocalStorage(
+        localStorageItemsEnum.ORDER
+      ) as IProductOrdered[];
 
-      let singleGoodsIndex = order.findIndex((i: IProductOrdered) => i.productId === itemId);
+      let singleGoodsIndex = order.findIndex(
+        (i: IProductOrdered) => i.productId === itemId
+      );
       order.splice(singleGoodsIndex, 1);
 
       state.userOrder.splice(singleGoodsIndex, 1);
 
-      localStorageHelper.setArrayToLocalStorage(localStorageItemsEnum.ORDER, order);
+      localStorageHelper.setArrayToLocalStorage(
+        localStorageItemsEnum.ORDER,
+        order
+      );
 
-      const cart = localStorageHelper.getArrayFromLocalStorage(localStorageItemsEnum.CART) as IProduct[];
+      const cart = localStorageHelper.getArrayFromLocalStorage(
+        localStorageItemsEnum.CART
+      ) as IProduct[];
 
       singleGoodsIndex = cart.findIndex((i: IProduct) => i._id === itemId);
       cart.splice(singleGoodsIndex, 1);
@@ -93,7 +121,10 @@ const cartSlice = createSlice({
       state.goods = [];
       state.goods = Object.assign([...cart]);
 
-      localStorageHelper.setArrayToLocalStorage(localStorageItemsEnum.CART, cart);
+      localStorageHelper.setArrayToLocalStorage(
+        localStorageItemsEnum.CART,
+        cart
+      );
     },
 
     deleteOrder: (state) => {
@@ -118,7 +149,10 @@ const cartSlice = createSlice({
   },
 });
 
-const {reducer: cartReducer, actions: { addToCart, changeOrder, changeOrderDeleteRecord, deleteOrder },} = cartSlice;
+const {
+  reducer: cartReducer,
+  actions: { addToCart, changeOrder, changeOrderDeleteRecord, deleteOrder },
+} = cartSlice;
 
 const cartActions = {
   addToCart,
