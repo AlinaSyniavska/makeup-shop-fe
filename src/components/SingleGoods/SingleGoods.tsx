3abc3ac4ad325cef95c9f1e8ec67782ba240da71
AlinaSyniavska <alina22syniavska@gmail.com>
2023-03-19 18:23:15 +0200

    change an event handler on inc/dec count goods in a cart

--- src/components/SingleGoods/SingleGoods.tsx
+++ src/components/SingleGoods/SingleGoods.tsx
@@ -1,105 +1,102 @@
-import React, { FC, useEffect, useState } from "react";
+import React, {FC, useEffect, useRef, useState} from "react";
 
 import style from "./SingleGoods.module.css";
-import { IProduct, IProductOrdered } from "../../interfaces";
-import { useAppDispatch, useAppSelector } from "../../hooks";
-import { cartActions } from "../../redux";
-import { localStorageHelper } from "../../helpers";
-import { localStorageItemsEnum } from "../../constants";
+import {IProduct, IProductOrdered} from "../../interfaces";
+import {useAppDispatch, useAppSelector} from "../../hooks";
+import {cartActions} from "../../redux";
+import {localStorageHelper} from "../../helpers";
+import {localStorageItemsEnum} from "../../constants";
 
 interface IProps {
-  item: IProduct;
-  index: number;
-  setTotal: Function;
+    item: IProduct;
+    index: number;
+    setTotal: Function;
 }
 
-const SingleGoods: FC<IProps> = ({ item, index, setTotal }) => {
-  const { goods, userOrder } = useAppSelector((state) => state.cartReducer);
-  const dispatch = useAppDispatch();
-  const [countGoods, setCountGoods] = useState<number>(1);
-  const [deletedGoods, setDeletedGoods] = useState<boolean>(false);
+const SingleGoods: FC<IProps> = ({item, index, setTotal}) => {
+    const {goods, userOrder} = useAppSelector((state) => state.cartReducer);
+    const dispatch = useAppDispatch();
+    const [countGoods, setCountGoods] = useState<number>(1);
+    const [deletedGoods, setDeletedGoods] = useState<boolean>(false);
+    const increment = useRef(null);
+    const decrement = useRef(null);
 
-  useEffect(() => {
-    const order = localStorageHelper.getArrayFromLocalStorage(localStorageItemsEnum.ORDER);
+    useEffect(() => {
+        const order = localStorageHelper.getArrayFromLocalStorage(localStorageItemsEnum.ORDER);
 
-    if (!deletedGoods) {
-      const singleGoods = order.find((orderItem: IProductOrdered) => orderItem.productId === item._id);
-      setCountGoods(singleGoods.count);
-    }
+        if (!deletedGoods) {
+            const singleGoods = order.find((orderItem: IProductOrdered) => orderItem.productId === item._id);
+            setCountGoods(singleGoods.count);
+        }
 
-    const totalCostArray = order.map((item: IProductOrdered) => item.count * item.cost);
+        const totalCostArray = order.map((item: IProductOrdered) => item.count * item.cost);
 
-    setTotal(
-        totalCostArray.reduce((accumulator: number, currentValue: number): number => {
-        return accumulator + currentValue;
-      }, 0)
+        setTotal(
+            totalCostArray.reduce((accumulator: number, currentValue: number): number => {
+                return accumulator + currentValue;
+            }, 0)
+        );
+    }, [goods, userOrder, deletedGoods]);
+
+    const changeCountGoods = (e: React.MouseEvent<HTMLButtonElement>) => {
+        let curCount = countGoods;
+
+        if (e.target === increment.current) {
+            curCount++;
+            if (curCount > item.total) {
+                curCount = countGoods;
+                alert("You cannot order more goods. \nThis quantity is not available.");
+            }
+        } else if (e.target === decrement.current) {
+            curCount--;
+            if (curCount <= 0) {
+                curCount = countGoods;
+            }
+        }
+
+        setCountGoods(curCount);
+        dispatch(cartActions.changeOrder({data: {itemId: item._id, curCount}}));
+    };
+
+    const deleteItemFromCart = () => {
+        setDeletedGoods(true);
+        dispatch(cartActions.changeOrderAfterDeleteCartItem({data: {itemId: item._id}}));
+    };
+
+    return (
+        <React.Fragment>
+            <div className={style.singleGoodsContainer}>
+                <div className={style.goodsName}>
+                    <p>{index + 1}.</p>
+                    <p>
+                        {item.name}
+                        <br/>
+                        {item.brand}
+                    </p>
+                </div>
+
+                <div className={style.orderInfo}>
+                    <div className={style.goodsQuantity}>
+                        <button className={style.btnChangeCount} onClick={changeCountGoods} ref={increment}>
+                            &#11014;
+                        </button>
+                        <div className={style.countGoods}>{countGoods}</div>
+                        <button className={style.btnChangeCount} onClick={changeCountGoods} ref={decrement}>
+                            &#11015;
+                        </button>
+                    </div>
+
+                    <div className={style.goodsPrice}>{item.price}</div>
+
+                    <div className={style.goodsTotalCost}>
+                        {(item.price * countGoods).toFixed(2)} {item.priceSign}
+                    </div>
+
+                    <button onClick={deleteItemFromCart}>DELETE</button>
+                </div>
+            </div>
+        </React.Fragment>
     );
-  }, [goods, userOrder, deletedGoods]);
-
-  const changeCountInc = () => {
-    let curCount = countGoods;
-
-    curCount++;
-    if (curCount > item.total) {
-      curCount = countGoods;
-      alert("You cannot order more goods. \nThis quantity is not available.");
-    }
-
-    setCountGoods(curCount);
-    dispatch(cartActions.changeOrder({ data: { itemId: item._id, curCount } }));
-  };
-
-  const changeCountDec = () => {
-    let curCount = countGoods;
-
-    curCount--;
-    if (curCount <= 0) {
-      curCount = countGoods;
-    }
-
-    setCountGoods(curCount);
-    dispatch(cartActions.changeOrder({ data: { itemId: item._id, curCount } }));
-  };
-
-  const deleteItemFromCart = () => {
-    setDeletedGoods(true);
-    dispatch(cartActions.changeOrderAfterDeleteCartItem({ data: { itemId: item._id } }));
-  };
-
-  return (
-    <React.Fragment>
-      <div className={style.singleGoodsContainer}>
-        <div className={style.goodsName}>
-          <p>{index + 1}.</p>
-          <p>
-            {item.name}
-            <br />
-            {item.brand}
-          </p>
-        </div>
-
-        <div className={style.orderInfo}>
-          <div className={style.goodsQuantity}>
-            <button className={style.btnChangeCount} id={"inc"} onClick={changeCountInc}>
-              &#11014;
-            </button>
-            <div className={style.countGoods}>{countGoods}</div>
-            <button className={style.btnChangeCount} onClick={changeCountDec}>
-              &#11015;
-            </button>
-          </div>
-
-          <div className={style.goodsPrice}>{item.price}</div>
-
-          <div className={style.goodsTotalCost}>
-            {(item.price * countGoods).toFixed(2)} {item.priceSign}
-          </div>
-
-          <button onClick={deleteItemFromCart}>DELETE</button>
-        </div>
-      </div>
-    </React.Fragment>
-  );
 };
 
-export { SingleGoods };
+export {SingleGoods};
