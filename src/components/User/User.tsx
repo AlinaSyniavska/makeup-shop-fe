cfab3da46644bf9c74a5ae17e9347d89aa1c96e0
AlinaSyniavska <alina22syniavska@gmail.com>
2023-03-22 23:11:06 +0200

    change a structure of User component

--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,49 +1,31 @@
-import { FC, useRef } from "react";
+import { FC } from "react";
 
 import style from "./User.module.css";
 import { IUser } from "../../interfaces";
-import { RegisterForm } from "../RegisterForm/RegisterForm";
-import { useAppDispatch, useAppSelector } from "../../hooks";
+import { useAppDispatch } from "../../hooks";
 import { userActions } from "../../redux";
 
 interface IProps {
-  user: IUser;
+    user: IUser;
 }
 
-const User: FC<IProps> = ({ user }) => {
-  const { name, surname, gender, age, email, phone } = user;
+const User: FC<IProps> = ({user}) => {
+    const {name, surname, gender, age, email, phone} = user;
+    const dispatch = useAppDispatch();
 
-  const btnUpdate = useRef<HTMLButtonElement>(null);
-  const { userForUpdate } = useAppSelector((state) => state.userReducer);
-  const dispatch = useAppDispatch();
+    return (
+        <div className={style.user}>
+            <div>Name: {name} {surname}</div>
+            <div>Age: {age}</div>
+            <div>Gender: {gender}</div>
+            <div>Phone: {phone}</div>
+            <div>Email: {email}</div>
 
-  return (
-    // <div className={style.userItem}>
-      <div className={style.user}>
-        <div>
-          Name: {name} {surname}
+            <button className={style.btnUpdate} onClick={() => dispatch(userActions.setUserForUpdate({user}))}>
+                Update
+            </button>
         </div>
-        <div>Age: {age}</div>
-        <div>Gender: {gender}</div>
-        <div>Phone: {phone}</div>
-        <div>Email: {email}</div>
-
-        <button
-          className={style.btnUpdate}
-          ref={btnUpdate}
-          onClick={() => {
-            dispatch(userActions.setUserForUpdate({ user }));
-          }}
-        >
-          Update
-        </button>
-      </div>
-
-      // <div className={style.userItemEdit} aria-disabled={!userForUpdate}>
-      //   <RegisterForm />
-      // </div>
-    // </div>
-  );
+    );
 };
 
 export { User };
